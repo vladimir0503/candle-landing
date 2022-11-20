b4fb8d19dd4b84f5adb8f3af436e08c202178115
Vladimir <vovan0503@mail.ru>
2022-11-20 17:44:41 +0300

    Commit

---
+++ src/js/common/submitForm.js
@@ -0,0 +1,5 @@
+const submitForm = async data => {
+    console.log(data, 'success');
+};
+
+export default submitForm;
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,7 +1,11 @@
+import submitForm from "./common/submitForm";
+
+
 const modal = () => {
 
     const modal = document.querySelector('.modal');
     const hideModalBtn = document.getElementById('hideModalBtn');
+    const form = modal.children[1];
 
     const showModal = () => {
         modal.classList.add('show')
@@ -18,6 +22,19 @@ const modal = () => {
     });
 
     // const timerId = setTimeout(showModal, 10000);
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const formData = new FormData(form);
+
+        const name = formData.get('name');
+        const mail = formData.get('mail');
+        const question = formData.get('question');
+
+        submitForm({name, mail, question})
+    });
+
 };
 
 export default modal;
