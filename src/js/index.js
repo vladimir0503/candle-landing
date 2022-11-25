65383b2a48b5b398f554578885cc450168a4bb4e
Vladimir <vovan0503@mail.ru>
2022-11-25 22:42:58 +0300

    Commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,12 @@
+import AOS from 'aos';
 import slider from "./slider";
 import smoothScroll from './smoothScroll';
 import modal from './modal';
 import burgerMenu from './burgerMenu';
+
+import 'aos/dist/aos.css';
+
+AOS.init({ once: false });
 
 smoothScroll();
 slider();
