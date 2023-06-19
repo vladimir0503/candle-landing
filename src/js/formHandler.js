f3f48e0f7ea04f8ad32f3f05573ac71c78c7589f
unknown <Vladimir>
2023-06-19 21:59:09 +0300

    Commit

--- src/js/formHandler.js
+++
@@ -1,8 +0,0 @@
-import submitForm from "./common/submitForm";
-
-const formHandler = () => {
-    const forms = document.querySelectorAll('form');
-    forms.forEach(form => submitForm(form));
-};
-
-export default formHandler;
--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,7 @@
 import AOS from 'aos';
-import slider from "./slider";
-import smoothScroll from './smoothScroll';
-import modal from './modal';
-import burgerMenu from './burgerMenu';
-import formHandler from './formHandler';
-import loadData from './common/loadData';
 
 import 'aos/dist/aos.css';
 
 window.addEventListener('DOMContentLoaded', () => {
-
     AOS.init({ once: true });
-
-    loadData();
-    smoothScroll();
-    // slider();
-    modal();
-    burgerMenu();
-    formHandler();
 });
