9a003acc400fc1d7db680e1ab90f2e51a1ff0a51
unknown <Vladimir>
2023-06-27 01:54:00 +0300

    Commit

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,25 +1,27 @@
 const burgerMenu = () => {
     const btn = document.querySelector('.header__content--burger-btn');
     const btnLine = document.querySelectorAll('.burger-btn-line');
+    const sideBar = document.querySelector('.side-bar');
 
     let menuInit = false;
 
-    const btnAnim = () => {
+    const toggleMenu = () => {
+        menuInit = !menuInit;
         if (menuInit) {
             btnLine[0].style.cssText = `transform: rotate(45deg) translate(6px, 7px)`;
             btnLine[1].style.opacity = '0';
             btnLine[2].style.cssText = `transform: rotate(-45deg) translate(7px, -7px);`;
+            sideBar.classList.add('show');
         } else {
             btnLine[0].style.cssText = `transform: rotate(0); top: auto`;
             btnLine[1].style.opacity = '1';
             btnLine[2].style.cssText = `transform: rotate(0); bottom: auto`;
+            sideBar.classList.remove('show');
         }
     };
 
-    btn.addEventListener('click', e => {
-        menuInit = !menuInit;
-        btnAnim();
-    });
+    btn.addEventListener('click', toggleMenu);
+    sideBar.addEventListener('click', toggleMenu);
 };
 
 export default burgerMenu;
